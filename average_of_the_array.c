27adf3db461feaa11bd2e5b6cc7882439813ed90
Karthikeya2026 <119730137+Karthikeya2026@users.noreply.github.com>
2022-12-10 13:32:06 +0530

    #include<stdio.h> int main(){     int n,i;     float s=0;     float e;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);          s=s+a[i];     }     e=s/n;     printf("%.2f",e); }

---
+++ average_of_the_array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    float s=0;
+    float e;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+         s=s+a[i];
+    }
+    e=s/n;
+    printf("%.2f",e);
+}
