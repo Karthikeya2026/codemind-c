8b0ab6e846cd990ca48ca107fe1fa93576337389
Karthikeya2026 <119730137+Karthikeya2026@users.noreply.github.com>
2022-12-12 07:13:31 +0530

    #include <stdio.h> int main() {  int days, years, weeks;  scanf("%d",&days);  years = days/365;  weeks = (days % 365)/7;  days = days- ((years*365) + (weeks*7));  printf("%d %d", years,weeks);
    }

--- Days_into_years,_weeks:.c
+++ Days_into_years,_weeks:.c
@@ -3,10 +3,10 @@ int main()
 {
  int days, years, weeks;
  scanf("%d",&days);
- years = days/365;
+ years = days/365; 
  weeks = (days % 365)/7;
  days = days- ((years*365) + (weeks*7));
  printf("%d
 %d", years,weeks);
-
+ 
 }
