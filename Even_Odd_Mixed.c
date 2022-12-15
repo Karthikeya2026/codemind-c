1bf73388fac526c93231050a21a6776799ffc2d4
Karthikeya2026 <119730137+Karthikeya2026@users.noreply.github.com>
2022-12-15 13:37:06 +0530

    #include<stdio.h> int main() {     int a,b,c,d,e,g,f;     scanf("%d",&a);     b=a%10;     c=a/10;     d=c%10;     e=c/10;     f=d%10;     g=f/10;     if(b%2==0&&d%2==0&&f%2==0) printf("Even");    else if(b%2==1&&d%2==1&&f%2==1) printf("Odd");    else printf("Mixed"); }

--- Even_Odd_Mixed.c
+++ Even_Odd_Mixed.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,d,e,f;
+    int a,b,c,d,e,g,f;
     scanf("%d",&a);
     b=a%10;
     c=a/10;
-    d=a%10;
-    e=a/10;
-    f=e%10;
+    d=c%10;
+    e=c/10;
+    f=d%10;
+    g=f/10;
     if(b%2==0&&d%2==0&&f%2==0) printf("Even");
-    else if(b%2==1&&d%2==1&&f%2==1) printf("Odd");
-    else printf("Mixed");
+   else if(b%2==1&&d%2==1&&f%2==1) printf("Odd");
+   else printf("Mixed");
 }
