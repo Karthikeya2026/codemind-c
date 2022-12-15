7525afbd9a2dbcbfc8388cd78f488e93bfd88c25
Karthikeya2026 <119730137+Karthikeya2026@users.noreply.github.com>
2022-12-15 15:51:45 +0530

    #include<stdio.h> int main() {     int a,i,j,s,b,c;     scanf("%d",&a);     for(i=0;i<a;i++)     {         scanf("%d %d",&b,&c);         {             for(j=0;j<a;j++)             s=b+c;         }         printf("%d ",s);     }
    }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j,s,b,c;
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    {
+        scanf("%d %d",&b,&c);
+        {
+            for(j=0;j<a;j++)
+            s=b+c;
+        }
+        printf("%d
+",s);
+    }
+    
+}
