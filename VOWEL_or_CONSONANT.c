06d4a89b57b5f291a50ea9e70ac04e53f3fa8a9e
Karthikeya2026 <119730137+Karthikeya2026@users.noreply.github.com>
2022-12-18 20:06:56 +0530

    #include<stdio.h> int main() {     char a;     scanf("%c",&a);     switch(a)     {         case  'a':         printf("VOWEL");         break;         case  'e':         printf("VOWEL");         break;         case  'i' :         printf("VOWEL");         break;         case  'o':         printf("VOWEL");         break;         case  'u' :         printf("VOWEL");         break;         default :         printf("CONSONANT");         break;     } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    switch(a)
+    {
+        case  'a':
+        printf("VOWEL");
+        break;
+        case  'e':
+        printf("VOWEL");
+        break;
+        case  'i' :
+        printf("VOWEL");
+        break;
+        case  'o':
+        printf("VOWEL");
+        break;
+        case  'u' :
+        printf("VOWEL");
+        break;
+        default :
+        printf("CONSONANT");
+        break;
+    }
+}
