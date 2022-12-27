e08200d0b7df4ade811709f701ed3107ccbd5c2c
Karthikeya2026 <119730137+Karthikeya2026@users.noreply.github.com>
2022-12-27 20:17:02 +0530

    #include <stdio.h> //#include <math.h> int main() {  int b,h,area;  scanf("%d%d",&b,&h);  area=(b*h)/2;  printf("%d",area); }

--- Area_of_a_triangle_.c
+++ Area_of_a_triangle_.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-//#include <math.h>
+//#include <math.h> 
 int main()
 {
-int b,h,area;
-scanf("%d%d",&b,&h);
-area=(b*h)/2;
-printf("%d",area);
+ int b,h,area;
+ scanf("%d%d",&b,&h);
+ area=(b*h)/2;
+ printf("%d",area);
 }
