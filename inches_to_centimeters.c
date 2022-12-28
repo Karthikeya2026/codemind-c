36d7b57078b4d20dc4cfd3941e4b473bfe403063
Karthikeya2026 <119730137+Karthikeya2026@users.noreply.github.com>
2022-12-28 18:56:29 +0530

    #include <stdio.h> int main() {  float inches, cm;
     // printf("Enter the length in inches: ");
     scanf("%f", &inches);
     
     cm = inches * 2.54;
     
     
     printf("%.2f",cm);
     // printf("%.2f", cm);
     return 0;
    }

--- inches_to_centimeters.c
+++ inches_to_centimeters.c
@@ -2,14 +2,14 @@
 int main()
 {
  float inches, cm;
-
-
+ 
+ 
  // printf("Enter the length in inches: ");
  scanf("%f", &inches);
-
+ 
  cm = inches * 2.54;
-
-
+ 
+ 
  printf("%.2f",cm);
  // printf("%.2f", cm);
  return 0;
