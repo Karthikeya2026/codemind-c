7d49c696099ddd837d47d90d27c2a2b97ceb12ab
Karthikeya2026 <119730137+Karthikeya2026@users.noreply.github.com>
2023-01-12 19:11:16 +0530

    #include<stdio.h> int main() {  float s,d,h,pf,gr;  scanf("%f%f%f",&s,&d,&h);  pf=(s*12)/100;  gr=s+pf+d+h;  printf("%.2f %.2f",pf,gr); }

--- Calculate_Gross_Salary_of_an_employee.c
+++ Calculate_Gross_Salary_of_an_employee.c
@@ -8,29 +8,3 @@ int main()
  printf("%.2f
 %.2f",pf,gr);
 }
-/*
-,pf,gr;
- scanf("%f%f%f",&#include<stdio.h>
-int main()
-{
- float s,d,h,pf,gr;
- scanf("%f%f%f",&s,&d,&h);
- pf=(s*12)/100;
- gr=s+pf+d+h;
- printf("%.2f
-%.2f",pf,gr);
-}s,&d,&h);
- pf=(s*12)/100;
- gr=s+pf+d+h;
- printf("%.2f
-%.2f",pf,gr);
-}#include<stdio.h>
-int main()
-{
- float s,d,h,pf,gr;
- scanf("%f%f%f",&s,&d,&h);
- pf=(s*12)/100;
- gr=s+pf+d+h;
- printf("%.2f
-%.2f",pf,gr);
-}*/
