d64540fc457e11bdb68fe43d5ca054ce6279f015
Karthikeya2026 <119730137+Karthikeya2026@users.noreply.github.com>
2023-01-12 19:11:00 +0530

    #include<stdio.h> int main() {  float s,d,h,pf,gr;  scanf("%f%f%f",&s,&d,&h);  pf=(s*12)/100;  gr=s+pf+d+h;  printf("%.2f %.2f",pf,gr); } /* ,pf,gr;  scanf("%f%f%f",&#include<stdio.h> int main() {  float s,d,h,pf,gr;  scanf("%f%f%f",&s,&d,&h);  pf=(s*12)/100;  gr=s+pf+d+h;  printf("%.2f %.2f",pf,gr); }s,&d,&h);  pf=(s*12)/100;  gr=s+pf+d+h;  printf("%.2f %.2f",pf,gr); }#include<stdio.h> int main() {  float s,d,h,pf,gr;  scanf("%f%f%f",&s,&d,&h);  pf=(s*12)/100;  gr=s+pf+d+h;  printf("%.2f %.2f",pf,gr); }*/

--- Calculate_Gross_Salary_of_an_employee.c
+++ Calculate_Gross_Salary_of_an_employee.c
@@ -1,10 +1,36 @@
 #include<stdio.h>
 int main()
 {
-float s,d,h,pf,gr;
-scanf("%f%f%f",&s,&d,&h);
-pf=(s*12)/100;
-gr=s+pf+d+h;
-printf("%.2f
+ float s,d,h,pf,gr;
+ scanf("%f%f%f",&s,&d,&h);
+ pf=(s*12)/100;
+ gr=s+pf+d+h;
+ printf("%.2f
 %.2f",pf,gr);
 }
+/*
+,pf,gr;
+ scanf("%f%f%f",&#include<stdio.h>
+int main()
+{
+ float s,d,h,pf,gr;
+ scanf("%f%f%f",&s,&d,&h);
+ pf=(s*12)/100;
+ gr=s+pf+d+h;
+ printf("%.2f
+%.2f",pf,gr);
+}s,&d,&h);
+ pf=(s*12)/100;
+ gr=s+pf+d+h;
+ printf("%.2f
+%.2f",pf,gr);
+}#include<stdio.h>
+int main()
+{
+ float s,d,h,pf,gr;
+ scanf("%f%f%f",&s,&d,&h);
+ pf=(s*12)/100;
+ gr=s+pf+d+h;
+ printf("%.2f
+%.2f",pf,gr);
+}*/
